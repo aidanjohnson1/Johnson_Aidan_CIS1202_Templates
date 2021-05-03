273ea7b12b1b5e7a6b2266aaf24661e6190b275c
Aidan Johnson <aidtomjohn624@gmail.com>
2021-05-02 22:36:52 -0400

    Added template function to accept return half of double and float values, as well as an int function to take and return half of an integer value.

--- Johnson_Aidan_CIS1202_Templates/Johnson_Aidan_CIS1202_Templates.cpp
+++ Johnson_Aidan_CIS1202_Templates/Johnson_Aidan_CIS1202_Templates.cpp
@@ -4,10 +4,23 @@
 #include <iostream>
 using namespace std;
 
+template <class T>
+T half(T) //Function definition for double and float values
+{
+	return (x / 2);
+}
+
+int half(int); //Function prototype for int values
+
 int main()
 {
 
 
 
 
+}
+
+int half(int x)
+{
+	return static_cast<int>(round((x / 2)));
 }

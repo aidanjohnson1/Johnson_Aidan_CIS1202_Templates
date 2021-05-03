7171b0a8e635cdcd701a660011c5218addc2be3f
Aidan Johnson <aidtomjohn624@gmail.com>
2021-05-02 22:45:21 -0400

    Fixed my int half function so that it would take in an int, divide by a double, round, and then convert back to an int to be returned. Also added main driver to test the functions.

--- Johnson_Aidan_CIS1202_Templates/Johnson_Aidan_CIS1202_Templates.cpp
+++ Johnson_Aidan_CIS1202_Templates/Johnson_Aidan_CIS1202_Templates.cpp
@@ -5,22 +5,25 @@
 using namespace std;
 
 template <class T>
-T half(T) //Function definition for double and float values
+T half(T nonIntValue) //Function definition for double and float values
 {
-	return (x / 2);
+	return (nonIntValue / 2);
 }
 
 int half(int); //Function prototype for int values
 
 int main()
 {
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
 
-
-
-
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
 }
 
-int half(int x)
+int half(int intValue)
 {
-	return static_cast<int>(round((x / 2)));
+	return static_cast<int>(round((intValue / 2.0))); //Divides the accepted int by 2, then properly rounds the result. Then converts the result to an integer value that can be returned
 }
